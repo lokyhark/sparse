165620d4c3a72fe4e322b2dbb192a31db6fbc087
Loky HARK <loky.hark@pm.me>
2026-06-21 05:51:33 +0200

    Reexport COLAMD appropriately

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,10 @@ mod int;
 mod scalar;
 
 // Public API.
-pub mod colamd;
 pub use coo::{CooMat, CooPat, CoordinateMatrix, CoordinatePattern};
 pub use csc::{CompressedSparseColumnMatrix, CompressedSparseColumnPattern, CscMat, CscPat};
 pub use scalar::Scalar;
+
+// Re-export API.
+#[doc(inline)]
+pub use sparse_colamd as colamd;
